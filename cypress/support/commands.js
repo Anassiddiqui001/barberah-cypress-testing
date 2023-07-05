f5469b6364a8c3b7c6e577bd5d104c97d0226ee7
ShayanJamil519 <shayanjamil500@gmail.com>
2023-07-05 07:33:59 +0500

    Appointment Testing Done

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,12 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", () => {
+  cy.visit("https://kind-gray-bluefish-tie.cyclic.app/");
+  cy.contains("Login").click();
+  cy.url().should("include", "/login");
+  cy.get("[id$=email]").type("bravo@gmail.com");
+  cy.get("[id$=password]").type("password");
+  cy.get("button").click();
+  cy.url().should(
+    "not.equal",
+    "https://kind-gray-bluefish-tie.cyclic.app/login"
+  );
+});
